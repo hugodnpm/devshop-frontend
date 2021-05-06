fd7b358360574fa155e32ee2e67fb22ba031c15a
hugodnpm <hugosc@gmail.com>
2021-05-06 14:47:00 -0300

    configurando token no frontend

--- panel/lib/graphql.js
+++ panel/lib/graphql.js
@@ -1,17 +1,103 @@
 import { useState } from 'react'
 import useSWR from 'swr'
 
+const getNewAccessToken = async () => {
+  const getAccessToken = {
+    query: `
+    mutation getAccessToken($refreshToken: String!){
+      accessToken(refreshToken: $refreshToken)
+    }
+    `,
+    variables: {
+      refreshToken: localStorage.getItem('refreshToken')
+    }
+  }
+  const resAccessToken = await fetch(process.env.NEXT_PUBLIC_API, {
+    headers,
+    method: 'POST',
+    body: JSON.stringify(getAccessToken)
+  })
+  const jsonAccessToken = await resAccessToken.json()
+  return jsonAccessToken
+}
+
 const fetcher = async query => {
+  const accessToken = localStorage.getItem('accessToken')
+  const headers = {
+    'Content-type': 'application/json'
+  }
+  if (accessToken) {
+    headers['authorization'] = 'Bearer ' + accessToken
+  }
   const res = await fetch(process.env.NEXT_PUBLIC_API, {
-    headers: {
-      'Content-type': 'application/json'
-    },
+    headers,
     method: 'POST',
     body: query
   })
   const json = await res.json()
+  if (!json.errors) {
+    return json
+  }
+  // pegar novo access Token
+  const jsonAccessToken = await getNewAccessToken()
+  if (jsonAccessToken.data) {
+    const newAccessToken = jsonAccessToken.data.accessToken
+    localStorage.setItem('accessToken', newAccessToken)
 
-  return json
+    const res2 = await fetch(process.env.NEXT_PUBLIC_API, {
+      headers: {
+        'Content-type': 'application/json',
+        authorization: 'Bearer ' + newAccessToken
+      },
+      method: 'POST',
+      body: query
+    })
+    const json2 = await res2.json()
+    if (!json2.errors) {
+      return json2
+    }
+  }
+  // enviar usuário tela de login:
+  window.location = '/'
+  return null
+}
+
+const uploader = async formData => {
+  const accessToken = localStorage.getItem('accessToken')
+  const headers = {}
+  if (accessToken) {
+    headers['authorization'] = 'Bearer ' + accessToken
+  }
+  const res = await fetch(process.env.NEXT_PUBLIC_API, {
+    headers,
+    method: 'POST',
+    body: formData
+  })
+  const json = await res.json()
+  if (!json.errors) {
+    return json
+  }
+  const jsonAccessToken = await getNewAccessToken()
+  if (jsonAccessToken.data) {
+    const newAccessToken = jsonAccessToken.data.accessToken
+    localStorage.setItem('accessToken', newAccessToken)
+
+    const res2 = await fetch(process.env.NEXT_PUBLIC_API, {
+      headers: {
+        authorization: 'Bearer ' + newAccessToken
+      },
+      method: 'POST',
+      body: formData
+    })
+
+    const json2 = await res2.json()
+    if (!json2.errors) {
+      return json2
+    }
+  }
+  // enviar usuário tela de login:
+  window.location = '/'
+  return null
 }
 
 const useQuery = queryStr => {
@@ -37,17 +123,6 @@ const useMutation = query => {
     } catch (err) {}
   }
   return [data, mutate]
-}
-
-const uploader = async formData => {
-  const res = await fetch(process.env.NEXT_PUBLIC_API, {
-    headers: {},
-    method: 'POST',
-    body: formData
-  })
-  const json = await res.json()
-
-  return json
 }
 
 const useUpload = query => {
@@ -77,44 +152,3 @@ const useUpload = query => {
 }
 
 export { useQuery, useMutation, fetcher, useUpload }
-
-/*
-const uploader = async formData => {
-  console.log(formData)
-  const res = await fetch(process.env.NEXT_PUBLIC_API, {
-    headers: {},
-    method: 'POST',
-    body: formData
-  })
-  const json = await res.json()
-
-  return json
-}
-
-const useUpload = query => {
-  const [data, setData] = useState(null)
-  const mutate = async variables => {
-    const mutation = {
-      query,
-      variables: {
-        ...variables,
-        file: null
-      }
-    }
-    const map = {
-      0: ['variables.file']
-    }
-    const formData = new FormData()
-    formData.append('operations', JSON.stringify(mutation))
-    formData.append('map', JSON.stringify(map))
-    formData.append(0, variables.file)
-    try {
-      const returnedData = await uploader(formData)
-      setData(returnedData)
-      return returnedData
-    } catch (err) {}
-  }
-  return [data, mutate]
-
-
-*/
--- panel/pages/index.js
+++ panel/pages/index.js
@@ -3,6 +3,8 @@ import { useFormik } from 'formik'
 import Button from '../components/Button'
 import { useMutation } from '../lib/graphql'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import Alert from '../components/Alert'
 
 const AUTH = `
     mutation auth($email: String!, $passwd: String!){
@@ -19,6 +21,7 @@ const AUTH = `
 const Index = () => {
   const router = useRouter()
   const [authData, auth] = useMutation(AUTH)
+  const [signInError, setSignInError] = useState(false)
   const form = useFormik({
     initialValues: {
       email: '',
@@ -31,15 +34,29 @@ const Index = () => {
         localStorage.setItem('accessToken', data.data.auth.accessToken)
         router.push('/dashboard')
       } else {
-        console.log(data.errors)
+        setSignInError(true)
       }
     }
   })
+  useEffect(() => {
+    let timer = setInterval(() => {
+      if (
+        localStorage.getItem('refreshToken') &&
+        localStorage.getItem('accessToken')
+      ) {
+        router.push('/dashboard')
+      }
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
 
   return (
     <div className='bg-grey-lighter min-h-screen flex flex-col'>
       <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2'>
         <div className='bg-white px-6 py-8 rounded shadow-md text-black w-full'>
+          {signInError && <Alert>E-mail e/ou Senha Inválidos</Alert>}
           <h1 className='mb-8 text-3xl text-center'>Sign In</h1>
           <form onSubmit={form.handleSubmit}>
             <Input
@@ -59,6 +76,7 @@ const Index = () => {
               name='passwd'
               errorMessage={form.errors.passwd}
             />
+
             <Button>Entrar</Button>
           </form>
         </div>
